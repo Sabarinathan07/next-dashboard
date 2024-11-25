9bb4040ddc3bf9170c86a5164d18acbb83b98db3
Sabarinathan R <r.sabarinathan02@gmail.com>
2024-11-25 23:04:33 +0530

    added dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+// const page = () => {
+//     return <div>Dashboard Page</div>;
+// };
+
+// export default page;
+
+export default function Page() {
+    return <p>Dashboard Page</p>;
+}
