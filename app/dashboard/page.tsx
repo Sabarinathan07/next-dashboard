80c510c79f4ea12fd78ef049c17cd3612b0de947
Sabarinathan R <r.sabarinathan02@gmail.com>
2024-11-26 23:54:53 +0530

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,10 +2,19 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue } from '../lib/data';
+import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '../lib/data';
 
 export default async function Page() {
     const revenue = await fetchRevenue();
+    const latestInvoices = await fetchLatestInvoices();
+      const {
+          numberOfInvoices,
+          numberOfCustomers,
+          totalPaidInvoices,
+          totalPendingInvoices,
+      } = await fetchCardData();
+
+    console.log(revenue, latestInvoices);
 
     return (
         <main>
@@ -13,7 +22,7 @@ export default async function Page() {
                 Dashboard
             </h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                {/* <Card
+                <Card
                     title="Collected"
                     value={totalPaidInvoices}
                     type="collected"
@@ -32,11 +41,11 @@ export default async function Page() {
                     title="Total Customers"
                     value={numberOfCustomers}
                     type="customers"
-                /> */}
+                />
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
                 <RevenueChart revenue={revenue} />
-                {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+                <LatestInvoices latestInvoices={latestInvoices} />
             </div>
         </main>
     );
