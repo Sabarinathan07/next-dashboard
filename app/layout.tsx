7f5e02e01bc113c76b83dcc7da6b77499a03ad8a
Sabarinathan R <r.sabarinathan02@gmail.com>
2024-12-23 21:14:01 +0530

    fix: build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,8 @@
-// layout.tsx
-import React from 'react';
-
-export const metadata = {
-    title: 'Your Page Title',
-    description: 'Your Page Description',
-};
-
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
     return (
         <html lang="en">
             <body>{children}</body>
